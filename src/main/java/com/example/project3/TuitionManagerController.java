03363f3bb53b6dc39e5244d9e9e87839b2f50aa2
David <davidhariantowu@gmail.com>
2023-03-18 17:47:17 -0400

    Initial Commit

--- src/main/java/com/example/project3/TuitionManagerController.java
+++ src/main/java/com/example/project3/TuitionManagerController.java
@@ -1,14 +1,32 @@
 package com.example.project3;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class TuitionManagerController {
     @FXML
-    private Label welcomeText;
+    private TextField fileName;
+    @FXML
+    private Button fileSubmit;
+    @FXML
+    private TextArea fileContents;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onFileSubmit() {
+        try {
+            Scanner scanner = new Scanner(new File("src/main/java/" + fileName.getText()));
+            while (scanner.hasNextLine()) {
+                fileContents.setText(fileContents.getText() + "\n" + scanner.nextLine());
+            }
+        }
+        catch(FileNotFoundException f) {
+            fileContents.setText(fileContents.getText() + "File \""+ fileName.getText() + "\" not found.");
+        }
     }
 }
